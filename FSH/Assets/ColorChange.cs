d3fd507f59ce223ee56142852574ed33b5ece008
Colin Yeo <colinyeo@alum.calarts.edu>
2020-04-29 19:14:39 -0700

    First Unity Upload

---
+++ FSH/Assets/ColorChange.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//[RequireComponent(typeof(AudioSource))]
+public class ColorChange : MonoBehaviour
+{
+
+    //public AudioClip clip;
+
+    public GameObject[] objectArray;
+
+    public int spot;
+
+    public GameObject red;
+    public GameObject blue;
+    public GameObject green;
+    public GameObject yellow;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+        spot = 0;
+
+        red.SetActive(true);
+        blue.SetActive(false);
+        green.SetActive(false);
+        yellow.SetActive(false);
+
+        objectArray = new GameObject[4];
+        objectArray[0] = red;
+        objectArray[1] = blue;
+        objectArray[2] = green;
+        objectArray[3] = yellow;
+
+    }
+
+	void OnTriggerExit(Collider other)
+	{
+        //AudioSource.Play();
+        if (other.gameObject.CompareTag("Fish"))
+        {
+            objectArray[spot].SetActive(false);
+            objectArray[spot + 1].SetActive(true);
+
+            if (spot == 3)
+            {
+                spot = 0;
+            }
+
+            else
+            {
+                spot++;
+            }
+        }
+	}
+}
---
+++ FSH/Assets/WanderingAI.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WanderingAI : MonoBehaviour
+{
+    public Camera cam;
+
+    public float wanderRadius;
+    public float wanderTimer;
+
+    public GameObject dude;
+
+    private Transform target;
+    private UnityEngine.AI.NavMeshAgent agent;
+    private float timer;
+
+    // Use this for initialization
+    void OnEnable()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        timer = wanderTimer;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.touchCount > 0)
+        {
+
+            Ray ray = cam.ScreenPointToRay(Input.GetTouch(0).position);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider.gameObject == dude)
+                {
+                    dude.transform.position = new Vector3(hit.point.x, transform.position.y, hit.point.z);
+                    return;
+                }
+            }
+        }
+
+        timer += Time.deltaTime;
+
+        if (timer >= wanderTimer)
+        {
+            Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
+            agent.SetDestination(newPos);
+            timer = 0;
+        }
+    }
+
+    public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
+    {
+        Vector3 randDirection = Random.insideUnitSphere * dist;
+
+        randDirection += origin;
+
+        UnityEngine.AI.NavMeshHit navHit;
+
+        UnityEngine.AI.NavMesh.SamplePosition(randDirection, out navHit, dist, layermask);
+
+        return navHit.position;
+    }
+}
